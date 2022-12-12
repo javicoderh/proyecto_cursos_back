c25376357eec8e680b796ce9520536ce1418906c
javicoderh <105983754+javicoderh@users.noreply.github.com>
2022-12-12 13:24:55 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import Router from "./routes/routes.js";
 import express from "express";
 import db from "./config/dataBase.js";
 import cors from "cors"
+import * as dotenv from "dotenv";
 
 const app = express();
 app.use(express.json())
@@ -18,5 +19,5 @@ try {
 
 app.use(Router);
 app.listen(process.env.PORT, () => {
-console.log('this seems like logic but it is magic on port:5000')
+console.log('welcome to cursimple, it is the magic oF WORKSHOPS')
 })
